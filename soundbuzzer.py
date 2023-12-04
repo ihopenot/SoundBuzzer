335d5ba7c08591722b5a5ad6d6c65ab2e2838441
Ihopenot <984430652@qq.com>
2023-12-04 22:12:18 +0800

    refactor code

--- soundbuzzer.py
+++ soundbuzzer.py
@@ -1,6 +1,8 @@
-from time import sleep
-from machine import Pin, PWM
+from time import sleep_us
+from machine import Pin, PWM, Timer
 from sys import platform
+
+from utils import Note
 
 silence = 48000
 inf = 1 << 30
@@ -13,7 +15,7 @@ def get_freq(note):
 
 # generic PWM
 class GPWM:
-    def __init__(self, pin, freq=440, duty_u16=0) -> None:
+    def __init__(self, pin, vol=None, freq=440, duty_u16=0) -> None:
         if platform == "rp2":
             self.pwm = PWM(pin)
             self.pwm.freq(freq)
@@ -21,11 +23,21 @@ class GPWM:
         else:
             self.pwm = PWM(pin, freq=freq, duty_u16=duty_u16)
 
-    def freq(self, freq):
-        self.pwm.freq(freq)
+        if vol:
+            self.vol = vol
+            self.vol.on()
 
-    def duty_u16(self, duty_u16):
-        self.pwm.duty_u16(duty_u16)
+    def freq(self, freq=None):
+        if freq:
+            self.pwm.freq(freq)
+        else:
+            return self.pwm.freq()
+
+    def duty_u16(self, duty_u16=None):
+        if duty_u16:
+            self.pwm.duty_u16(duty_u16)
+        else:
+            return self.pwm.duty_u16()
 
 
 class SoundManager:
@@ -33,18 +45,26 @@ class SoundManager:
         self.keyframe = inf
         self.pwms = []
         for pin in pins:
-            self.pwms.append(GPWM(Pin(pin), freq=silence, duty_u16=32768))
+            self.pwms.append(
+                GPWM(Pin(pin), vol=Pin(pin - 1, Pin.OUT), freq=silence, duty_u16=32768)
+            )
         self.avi = [i for i in range(len(self.pwms))]  # 可用pin
         self.stop = [inf for _ in range(len(self.pwms))]  # 当前音的停止时间
         self.track = [-1 for _ in range(len(self.pwms))]
 
-    def need_int(self):
-        return self.keyframe
+    def init(self):
+        for pwm in self.pwms:
+            pwm.freq(silence)
 
-    def update_keyframe(self):
-        self.keyframe = min(self.stop)
+    def update(self, time):
+        for i in range(len(self.pwms)):
+            if time >= self.stop[i]:
+                self.pwms[i].freq(silence)
+                self.avi.append(i)
+                self.stop[i] = inf
+        return min(self.stop)
 
-    def add(self, inst, nodeid, start, duration, track):  # start aka time
+    def add(self, note):  # start aka time
         # TODO: inst
         idx = -1
         if len(self.avi) == 0:  # try to stop some same track note
@@ -61,18 +81,9 @@ class SoundManager:
         else:
             idx = self.avi.pop()
 
-        self.pwms[idx].freq(get_freq(nodeid))
-        self.stop[idx] = start + duration
-        self.track[idx] = track
-        self.update_keyframe()
-
-    def interval(self, time):
-        for i in range(len(self.pwms)):
-            if time >= self.stop[i]:
-                self.pwms[i].freq(silence)
-                self.avi.append(i)
-                self.stop[i] = inf
-        self.update_keyframe()
+        self.pwms[idx].freq(get_freq(note.noteid))
+        self.stop[idx] = note.start + note.duration
+        self.track[idx] = note.track
 
     def silence(self):
         for i in range(len(self.pwms)):
@@ -85,60 +96,79 @@ class SBConfig:
         self.map = {track: None for track in tracks}
         self.snds = set()
 
+    def init(self):
+        for snd in self.snds:
+            snd.init()
+
+    def update(self, time):
+        return min([snd.update(time) for snd in self.snds])
+
     def set_snd(self, tracks, snd):
         for track in tracks:
             self.map[track] = snd
         self.snds.add(snd)
 
-    def snd_need_int(self):
-        return min([snd.need_int() for snd in self.snds])
-
-    def snd_interval(self, time):
-        for snd in self.snds:
-            snd.interval(time)
-
     def silence(self):
         for snd in self.snds:
             snd.silence()
 
+    def add(self, note: Note):
+        playsnd = self.map[note.track]
+        playsnd.add(note)
+
 
 class SB:
-    def __init__(self, filename, sbconfig: SBConfig) -> None:
+    def __init__(self, filename, sbconfig: SBConfig = None) -> None:
         self.tempo = 500000
         self.notes = []
+        self.conf = sbconfig
 
         self.time = 0
-        for line in open(filename).readlines():
-            args = line.split(" ")
-            if args[0] == "tempo":
-                self.tempo = int(args[1])
-                print("tempo: {}".format(self.tempo))
-            elif args[0] == "note":
-                start = int(args[3])
-                noteid = int(args[2])
-                track = int(args[5])
-                duration = int(args[4])
+        with open(filename) as f:
+            line = "START"
+            while line != "":
+                line = f.readline()
+                args = line.split(" ")
+                if args[0] == "tempo":
+                    tempo = int(args[1])
+                    self.tempo = tempo
+                    print("tempo: {}".format(tempo))
+                elif args[0] == "note":
+                    self.notes.append(Note.load(args))
 
-                playsnd = sbconfig.map[track]
-                kf = sbconfig.snd_need_int()
-                while kf <= start:
-                    self.goto(kf)
-                    sbconfig.snd_interval(self.time)
-                    kf = sbconfig.snd_need_int()
+    def init(self):
+        self.time = 0
+        self.cur_note = 0
+        self.conf.init()
 
-                self.goto(start)
-                print(self.time, noteid)
-                playsnd.add(int(args[1]), noteid, start, min(duration, 960), track)
+    def update(self, time):
+        while (
+            self.cur_note < len(self.notes) and self.notes[self.cur_note].start == time
+        ):
+            self.conf.add(self.notes[self.cur_note])
+            self.cur_note += 1
 
-        kf = sbconfig.snd_need_int()
-        while kf <= start:
-            self.goto(kf)
-            sbconfig.snd_interval(self.time)
-            kf = sbconfig.snd_need_int()
+        next_time = self.conf.update(time)
 
-        sbconfig.silence()
+        return min(
+            next_time,
+            self.notes[self.cur_note].start if self.cur_note < len(self.notes) else inf,
+        )
+
+    def play(self):
+        self.init()
+
+        next_time = self.update(self.time)
+        while next_time < inf:
+            self.goto(next_time)
+            next_time = self.update(self.time)
+
+        self.silence()
+
+    def silence(self):
+        self.conf.silence()
 
     def goto(self, time):
-        tosleep = (time - self.time) * self.tempo / 1000000 / 480
-        sleep(tosleep)
+        tosleep = (time - self.time) * self.tempo / 480
+        sleep_us(int(tosleep))
         self.time = time
