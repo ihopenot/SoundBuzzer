b05592dfc687206800dfe2272500d16529171a66
Ihopenot <984430652@qq.com>
2023-12-04 22:13:15 +0800

    update example

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 import soundbuzzer
 
-snd0 = soundbuzzer.SoundManager([6])
-snd1 = soundbuzzer.SoundManager([7])
-conf = soundbuzzer.SBConfig([0, 1])
+snd0 = soundbuzzer.SoundManager([3, 5])
+snd1 = soundbuzzer.SoundManager([7, 9])
+conf = soundbuzzer.SBConfig([0, 1, 2])
 conf.set_snd([0], snd0)
-conf.set_snd([1], snd1)
+conf.set_snd([1, 2], snd1)
 
-soundbuzzer.SB("example.sb", conf)
+sb = soundbuzzer.SB("weather", conf)
