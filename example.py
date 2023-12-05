9aa2cf83ec5d14110a065b2089d95adcfbf62573
Ihopenot <984430652@qq.com>
2023-12-05 21:13:39 +0800

    update example

--- example.py
+++ example.py
@@ -1,7 +1,13 @@
 import soundbuzzer
 
-snd0 = soundbuzzer.SoundManager([3, 5])
-snd1 = soundbuzzer.SoundManager([7, 9])
+b1 = soundbuzzer.Buzzer(soundbuzzer.Buzzer.SOFT, 7, 20)
+b2 = soundbuzzer.Buzzer(soundbuzzer.Buzzer.SOFT, 9, 19)
+b3 = soundbuzzer.Buzzer(soundbuzzer.Buzzer.SOFT, 11, 18)
+b4 = soundbuzzer.Buzzer(soundbuzzer.Buzzer.SOFT, 13, 17)
+b5 = soundbuzzer.Buzzer(soundbuzzer.Buzzer.SOFT, 15, 16)
+
+snd0 = soundbuzzer.SoundManager([b1, b2])
+snd1 = soundbuzzer.SoundManager([b3, b4, b5])
 conf = soundbuzzer.SBConfig([0, 1, 2])
 conf.set_snd([0], snd0)
 conf.set_snd([1, 2], snd1)
